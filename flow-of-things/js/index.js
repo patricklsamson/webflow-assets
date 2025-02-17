1af0dd5103da72edc60e086de0eb370a303f2b9b
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2025-02-18 04:15:27 +0800

    Improve stylesheet injection

--- flow-of-things/js/index.js
+++ flow-of-things/js/index.js
@@ -1,6 +1,3 @@
-injectStyles(
-  [
-    "global-styles.css"
-  ],
-  "https://cdn.jsdelivr.net/gh/patricklsamson/webflow-assets@latest/flow-of-things/css"
-);
+injectStylesheets([
+  "https://cdn.jsdelivr.net/gh/patricklsamson/webflow-assets@latest/flow-of-things/css/global-styles.css"
+]);
--- flow-of-things/js/utility.js
+++ flow-of-things/js/utility.js
@@ -1,14 +1,13 @@
-const injectStyles = (stylesheets, baseUrl) => {
+const injectStylesheets = (stylesheets) => {
   stylesheets.forEach((stylesheet) => {
     const link = document.createElement("link");
 
     link.setAttribute("rel", "stylesheet");
 
-    const stylesheetLink = `${baseUrl ? `${baseUrl}/` : ""}${stylesheet}?v=${
+    link.setAttribute("href", `${stylesheet}?v=${
       Math.random().toString().substring(2)
-    }`;
+    }`);
 
-    link.setAttribute("href", stylesheetLink);
     document.head.appendChild(link);
   });
 };
