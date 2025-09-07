ba5bdade2a0e81051000c79d15bf65ca70b75ed5
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2025-09-07 20:39:13 +0800

    Reorganize scripts

--- global/js/index.js
+++ global/js/index.js
@@ -20,224 +20,6 @@ const injectSourceCodes = (sourceCodes) => {
   }
 };
 
-const resolveSpans = () => {
-  const texts = document.querySelectorAll("[data-span_style]");
-
-  if (texts.length > 0) {
-    texts.forEach((text) => {
-      const spanClasses = text.dataset.span_style.split("|");
-      let index = 0;
-
-      const resolvedText = text.innerHTML.replace(
-        /\|([^|]+?)\|/g,
-        (_, content) => {
-          const spanClass = spanClasses[index];
-
-          if (spanClasses.length > 1) {
-            index++;
-          }
-
-          return spanClass
-            ? `<span class="${spanClass}">${content}</span>`
-            : content;
-        }
-      );
-
-      text.innerHTML = resolvedText;
-    });
-  }
-};
-
-const initMirrorClick = () => {
-  const triggers = document.querySelectorAll("[data-mirror_trigger]");
-
-  if (triggers.length > 0) {
-    triggers.forEach((trigger) => {
-      trigger.addEventListener("click", function (e) {
-        e.preventDefault();
-
-        const { mirror_trigger, mirror_delay } = this.dataset;
-
-        const targets = document.querySelectorAll(
-          `[data-mirror_target="${mirror_trigger}"]`
-        );
-
-        if (targets.length > 0) {
-          setTimeout(() => {
-            targets.forEach((target) => {
-              if (
-                target.classList.contains("w-dropdown-toggle") &&
-                target.classList.contains("w--open")
-              ) {
-                return false;
-              }
-
-              target.dispatchEvent(new Event("mousedown"));
-              target.dispatchEvent(new Event("mouseup"));
-              target.click();
-            });
-          }, parseInt(mirror_delay) || 0);
-        }
-      });
-    });
-  }
-};
-
-const openActiveAccordions = () => {
-  const accordionHeaders = document.querySelectorAll(
-    "[data-active_accordion='true']"
-  );
-
-  if (accordionHeaders.length > 0) {
-    accordionHeaders.forEach((header) => {
-      header.dispatchEvent(new Event("mousedown"));
-      header.dispatchEvent(new Event("mouseup"));
-      header.click();
-    });
-  }
-};
-
-const initBottomAnchors = (breakpoint = 992) => {
-  const triggers = Array.from(
-    document.querySelectorAll("[data-bottom_href]")
-  ).filter((trigger) => trigger.dataset.bottom_href);
-
-  if (triggers.length > 0) {
-    const runOnMatch = (media) => {
-      if (media.matches) {
-        triggers.forEach((trigger) => {
-          trigger.addEventListener("click", function (e) {
-            e.preventDefault();
-
-            const { bottom_href, bottom_delay } = this.dataset;
-
-            const target = document.querySelector(
-              `[data-bottom_id="${bottom_href}"]`
-            );
-
-            if (target) {
-              setTimeout(() => {
-                window.scrollTo({
-                  top: (
-                    target.getBoundingClientRect().top + window.scrollY
-                  ) - window.innerHeight,
-                  behavior: "smooth",
-                });
-              }, parseInt(bottom_delay) || 0);
-            }
-          });
-        });
-      }
-    };
-
-    const media = window.matchMedia(
-      `only screen and (${breakpoint >= 0 ? "min" : "max"}-width: ${
-        breakpoint >= 0 ? breakpoint : breakpoint * -1
-      }px)`
-    );
-
-    runOnMatch(media);
-
-    window.addEventListener("resize", () => {
-      runOnMatch(media);
-    });
-
-    media.addEventListener("change", runOnMatch);
-  }
-};
-
-const initTimeToRead = (
-  wordsBefore,
-  wordsAfter = "read",
-  unit = "min",
-  wordsPerMinute = 100
-) => {
-  const timeSources = document.querySelectorAll("[data-time_source]");
-
-  if (timeSources.length > 0) {
-    timeSources.forEach((source) => {
-      const words = source.innerText.split(" ").length;
-      const images = source.querySelectorAll("img").length;
-      const videos = source.querySelectorAll("iframe").length;
-      const { time_video, time_source } = source.dataset;
-
-      const minutes = Math.floor(
-        (words / wordsPerMinute) +
-        ((images * 10) / 60) +
-        (time_video ? parseInt(time_video) : videos * 4)
-      );
-
-      const timeToRead = minutes > 1 ? `${minutes} ${unit}s` : `1 ${unit}`;
-
-      const timeTarget = document.querySelector(
-        `[data-time_target="${time_source}"]`
-      );
-
-      timeTarget.innerHTML = `${wordsBefore ? `${wordsBefore} ` : ""}${
-        timeToRead
-      }${wordsAfter ? ` ${wordsAfter}` : ""}`;
-    });
-  }
-};
-
-const initSocialShare = () => {
-  const urlSharers = document.querySelectorAll("[data-share_url]");
-
-  if(urlSharers.length > 0) {
-    urlSharers.forEach((sharer) => {
-      const { share_url, slug } = sharer.dataset;
-      const { origin, href } = window.location;
-      let socialMediaUrl = null;
-      let defaultShare = false;
-
-      switch (share_url) {
-        case "facebook":
-          socialMediaUrl = "https://www.facebook.com/sharer/sharer.php?u=";
-
-          break;
-        case "twitter":
-        case "x":
-          socialMediaUrl = "https://twitter.com/intent/tweet?url=";
-
-          break;
-        case "linkedin":
-          socialMediaUrl = "https://www.linkedin.com/sharing/share-offsite/?url=";
-
-          break;
-        default:
-          socialMediaUrl = share_url;
-      }
-
-      if (share_url === "clipboard") {
-        sharer.addEventListener("click", function (e) {
-          e.preventDefault();
-
-          const { slug } = this.dataset;
-          const { origin, href } = window.location;
-
-          navigator.clipboard.writeText(`${slug ? `${origin}${slug}` : href}`);
-        });
-      } else {
-        sharer.href = `${slug ? `${socialMediaUrl}${origin}${slug}` : href}`;
-        sharer.target = "_blank";
-        sharer.rel = "noopener noreferrer";
-      }
-    });
-  }
-};
-
-const removeInvisibleElements = () => {
-  const invisibleElements = document.querySelectorAll(
-    ".w-condition-invisible"
-  );
-
-  if (invisibleElements.length > 0) {
-    invisibleElements.forEach((element) => {
-      element.remove();
-    });
-  }
-};
-
 const requestApi = async (
   url,
   {
@@ -480,6 +262,16 @@ const initFormSubmit = (
   }
 };
 
+const injectSvgs = () => {
+  const svgTargets = document.querySelectorAll("[data-svg='target']");
+
+  if (svgTargets.length > 0) {
+    svgTargets.forEach((wrapper) => {
+      wrapper.parentNode.innerHTML = wrapper.innerText;
+    });
+  }
+};
+
 const lazyLoadAssets = () => {
   const assets = Array.from(document.querySelectorAll('[data-lazy="true"]'));
 
@@ -545,40 +337,6 @@ const lazyLoadAssets = () => {
   }
 };
 
-const injectSvgs = () => {
-  const svgTargets = document.querySelectorAll("[data-svg='target']");
-
-  if (svgTargets.length > 0) {
-    svgTargets.forEach((wrapper) => {
-      wrapper.parentNode.innerHTML = wrapper.innerText;
-    });
-  }
-};
-
-const initMediaMatch = (breakpoint, onMatch, onUnmatch) => {
-  const runOnMatch = (media) => {
-    if (media.matches) {
-      onMatch && onMatch();
-    } else {
-      onUnmatch && onUnmatch();
-    }
-  };
-
-  const media = window.matchMedia(
-    `only screen and (${breakpoint >= 0 ? "min" : "max"}-width: ${
-      breakpoint >= 0 ? breakpoint : breakpoint * -1
-    }px)`
-  );
-
-  runOnMatch(media);
-
-  window.addEventListener("resize", () => {
-    runOnMatch(media);
-  });
-
-  media.addEventListener("change", runOnMatch);
-};
-
 const initResponsiveGsapAnimations = () => {
   const animationElements = Array.from(document.querySelectorAll(
     "[data-animation][data-breakpoint]"
@@ -623,6 +381,30 @@ const initResponsiveGsapAnimations = () => {
       media.addEventListener("change", runOnMatch);
     });
   }
+};
+
+const initMediaMatch = (breakpoint, onMatch, onUnmatch) => {
+  const runOnMatch = (media) => {
+    if (media.matches) {
+      onMatch && onMatch();
+    } else {
+      onUnmatch && onUnmatch();
+    }
+  };
+
+  const media = window.matchMedia(
+    `only screen and (${breakpoint >= 0 ? "min" : "max"}-width: ${
+      breakpoint >= 0 ? breakpoint : breakpoint * -1
+    }px)`
+  );
+
+  runOnMatch(media);
+
+  window.addEventListener("resize", () => {
+    runOnMatch(media);
+  });
+
+  media.addEventListener("change", runOnMatch);
 };
 
 const initMasonry = (identifier, configSet) => {
@@ -736,6 +518,132 @@ const initMasonry = (identifier, configSet) => {
     media.addEventListener("change", initOnMatch);
   } else {
     masonries = handleInitMasonries();
+  }
+};
+
+const initBottomAnchors = (breakpoint = 992) => {
+  const triggers = Array.from(
+    document.querySelectorAll("[data-bottom_href]")
+  ).filter((trigger) => trigger.dataset.bottom_href);
+
+  if (triggers.length > 0) {
+    const runOnMatch = (media) => {
+      if (media.matches) {
+        triggers.forEach((trigger) => {
+          trigger.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            const { bottom_href, bottom_delay } = this.dataset;
+
+            const target = document.querySelector(
+              `[data-bottom_id="${bottom_href}"]`
+            );
+
+            if (target) {
+              setTimeout(() => {
+                window.scrollTo({
+                  top: (
+                    target.getBoundingClientRect().top + window.scrollY
+                  ) - window.innerHeight,
+                  behavior: "smooth",
+                });
+              }, parseInt(bottom_delay) || 0);
+            }
+          });
+        });
+      }
+    };
+
+    const media = window.matchMedia(
+      `only screen and (${breakpoint >= 0 ? "min" : "max"}-width: ${
+        breakpoint >= 0 ? breakpoint : breakpoint * -1
+      }px)`
+    );
+
+    runOnMatch(media);
+
+    window.addEventListener("resize", () => {
+      runOnMatch(media);
+    });
+
+    media.addEventListener("change", runOnMatch);
+  }
+};
+
+const resolveSpans = () => {
+  const texts = document.querySelectorAll("[data-span_style]");
+
+  if (texts.length > 0) {
+    texts.forEach((text) => {
+      const spanClasses = text.dataset.span_style.split("|");
+      let index = 0;
+
+      const resolvedText = text.innerHTML.replace(
+        /\|([^|]+?)\|/g,
+        (_, content) => {
+          const spanClass = spanClasses[index];
+
+          if (spanClasses.length > 1) {
+            index++;
+          }
+
+          return spanClass
+            ? `<span class="${spanClass}">${content}</span>`
+            : content;
+        }
+      );
+
+      text.innerHTML = resolvedText;
+    });
+  }
+};
+
+const initMirrorClick = () => {
+  const triggers = document.querySelectorAll("[data-mirror_trigger]");
+
+  if (triggers.length > 0) {
+    triggers.forEach((trigger) => {
+      trigger.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const { mirror_trigger, mirror_delay } = this.dataset;
+
+        const targets = document.querySelectorAll(
+          `[data-mirror_target="${mirror_trigger}"]`
+        );
+
+        if (targets.length > 0) {
+          setTimeout(() => {
+            targets.forEach((target) => {
+              if (
+                target.classList.contains("w-dropdown-toggle") &&
+                target.classList.contains("w--open")
+              ) {
+                return false;
+              }
+
+              target.dispatchEvent(new Event("mousedown"));
+              target.dispatchEvent(new Event("mouseup"));
+              target.click();
+            });
+          }, parseInt(mirror_delay) || 0);
+        }
+      });
+    });
+  }
+};
+
+const openActiveAccordions = () => {
+  const accordionHeaders = document.querySelectorAll(
+    "[data-active_accordion='true']"
+  );
+
+  if (accordionHeaders.length > 0) {
+    accordionHeaders.forEach((header) => {
+      header.dispatchEvent(new Event("mousedown"));
+      header.dispatchEvent(new Event("mouseup"));
+      header.click();
+    });
   }
 };
 
@@ -879,58 +787,6 @@ const toggleSlideOverlays = (swiper) => {
   });
 };
 
-const initAutoplayTabs = () => {
-  const setActiveTab = (tabLinks, index) => {
-    tabLinks.forEach((tabLink, i) => {
-      const tabPaneId = tabLink.href.split("#")[1];
-      const tabPane = document.getElementById(tabPaneId);
-
-      if (index === i) {
-        tabLink.classList.add("w--current");
-        tabLink.setAttribute("aria-selected", "true");
-        tabLink.removeAttribute("tabindex");
-        tabPane.classList.add("w--tab-active");
-      } else {
-        tabLink.classList.remove("w--current");
-        tabLink.setAttribute("aria-selected", "false");
-        tabLink.setAttribute("tabindex", "-1");
-        tabPane.classList.remove("w--tab-active");
-      }
-    });
-  };
-
-  const resolveIndex = (index, array) => {
-    return index < array.length - 1 ? index + 1 : 0;
-  };
-
-  const tabs = document.querySelectorAll("[data-autoplay_tab]");
-
-  tabs.forEach((tab) => {
-    const tabLinks = tab.querySelectorAll(".w-tab-link");
-    const timeInterval = parseInt(tab.dataset.autoplay_tab) || 2000;
-    let currentIndex = 0;
-
-    let tabInterval = setInterval(() => {
-      setActiveTab(tabLinks, currentIndex);
-      currentIndex = resolveIndex(currentIndex, tabLinks);
-    }, timeInterval);
-
-    tabLinks.forEach((tabLink, i) => {
-      tabLink.addEventListener("click", (e) => {
-        e.preventDefault();
-        clearInterval(tabInterval);
-        setActiveTab(tabLinks, i);
-        currentIndex = resolveIndex(i, tabLinks);
-
-        tabInterval = setInterval(() => {
-          setActiveTab(tabLinks, currentIndex);
-          currentIndex = resolveIndex(currentIndex, tabLinks);
-        }, timeInterval)
-      });
-    });
-  });
-};
-
 const initToc = () => {
   const tocSources = document.querySelectorAll("[data-toc_source]");
 
@@ -1005,6 +861,149 @@ const initToc = () => {
           parentTarget.appendChild(levelTargetClone);
         }
       });
+    });
+  }
+};
+
+const initAutoplayTabs = () => {
+  const setActiveTab = (tabLinks, index) => {
+    tabLinks.forEach((tabLink, i) => {
+      const tabPaneId = tabLink.href.split("#")[1];
+      const tabPane = document.getElementById(tabPaneId);
+
+      if (index === i) {
+        tabLink.classList.add("w--current");
+        tabLink.setAttribute("aria-selected", "true");
+        tabLink.removeAttribute("tabindex");
+        tabPane.classList.add("w--tab-active");
+      } else {
+        tabLink.classList.remove("w--current");
+        tabLink.setAttribute("aria-selected", "false");
+        tabLink.setAttribute("tabindex", "-1");
+        tabPane.classList.remove("w--tab-active");
+      }
+    });
+  };
+
+  const resolveIndex = (index, array) => {
+    return index < array.length - 1 ? index + 1 : 0;
+  };
+
+  const tabs = document.querySelectorAll("[data-autoplay_tab]");
+
+  tabs.forEach((tab) => {
+    const tabLinks = tab.querySelectorAll(".w-tab-link");
+    const timeInterval = parseInt(tab.dataset.autoplay_tab) || 1000;
+    let currentIndex = 0;
+
+    let tabInterval = setInterval(() => {
+      setActiveTab(tabLinks, currentIndex);
+      currentIndex = resolveIndex(currentIndex, tabLinks);
+    }, timeInterval);
+
+    tabLinks.forEach((tabLink, i) => {
+      tabLink.addEventListener("click", (e) => {
+        e.preventDefault();
+        clearInterval(tabInterval);
+        setActiveTab(tabLinks, i);
+        currentIndex = resolveIndex(i, tabLinks);
+
+        tabInterval = setInterval(() => {
+          setActiveTab(tabLinks, currentIndex);
+          currentIndex = resolveIndex(currentIndex, tabLinks);
+        }, timeInterval)
+      });
+    });
+  });
+};
+
+const initTimeToRead = (
+  wordsBefore,
+  wordsAfter = "read",
+  unit = "min",
+  wordsPerMinute = 100
+) => {
+  const timeSources = document.querySelectorAll("[data-time_source]");
+
+  if (timeSources.length > 0) {
+    timeSources.forEach((source) => {
+      const words = source.innerText.split(" ").length;
+      const images = source.querySelectorAll("img").length;
+      const videos = source.querySelectorAll("iframe").length;
+      const { time_video, time_source } = source.dataset;
+
+      const minutes = Math.floor(
+        (words / wordsPerMinute) +
+        ((images * 10) / 60) +
+        (time_video ? parseInt(time_video) : videos * 4)
+      );
+
+      const timeToRead = minutes > 1 ? `${minutes} ${unit}s` : `1 ${unit}`;
+
+      const timeTarget = document.querySelector(
+        `[data-time_target="${time_source}"]`
+      );
+
+      timeTarget.innerHTML = `${wordsBefore ? `${wordsBefore} ` : ""}${
+        timeToRead
+      }${wordsAfter ? ` ${wordsAfter}` : ""}`;
+    });
+  }
+};
+
+const initSocialShare = () => {
+  const urlSharers = document.querySelectorAll("[data-share_url]");
+
+  if(urlSharers.length > 0) {
+    urlSharers.forEach((sharer) => {
+      const { share_url, slug } = sharer.dataset;
+      const { origin, href } = window.location;
+      let socialMediaUrl = null;
+
+      switch (share_url) {
+        case "facebook":
+          socialMediaUrl = "https://www.facebook.com/sharer/sharer.php?u=";
+
+          break;
+        case "twitter":
+        case "x":
+          socialMediaUrl = "https://twitter.com/intent/tweet?url=";
+
+          break;
+        case "linkedin":
+          socialMediaUrl = "https://www.linkedin.com/sharing/share-offsite/?url=";
+
+          break;
+        default:
+          socialMediaUrl = share_url;
+      }
+
+      if (share_url === "clipboard") {
+        sharer.addEventListener("click", function (e) {
+          e.preventDefault();
+
+          const { slug } = this.dataset;
+          const { origin, href } = window.location;
+
+          navigator.clipboard.writeText(`${slug ? `${origin}${slug}` : href}`);
+        });
+      } else {
+        sharer.href = `${slug ? `${socialMediaUrl}${origin}${slug}` : href}`;
+        sharer.target = "_blank";
+        sharer.rel = "noopener noreferrer";
+      }
+    });
+  }
+};
+
+const removeInvisibleElements = () => {
+  const invisibleElements = document.querySelectorAll(
+    ".w-condition-invisible"
+  );
+
+  if (invisibleElements.length > 0) {
+    invisibleElements.forEach((element) => {
+      element.remove();
     });
   }
 };
